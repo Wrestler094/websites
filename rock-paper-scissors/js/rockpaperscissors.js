0fb4460473d2c57117c2710fa60fb975344cc4cb
Colin McNamara <coolinmc6@gmail.com>
2016-07-25 22:50:33 -0400

    Updated RPS and my JS playaround

--- rock-paper-scissors/js/rockpaperscissors.js
+++ rock-paper-scissors/js/rockpaperscissors.js
@@ -1,91 +1,170 @@
 // rockpaperscissors.js
+
 var playerScore = 0;
 var computerScore = 0;
 var matches = 0;
-var playerChoice = ""
-var player = ""
-var computer = ""
+var playerChoice = "";
+var player = "";
+var e = document.getElementById("player-display");
+var c = document.getElementById("computer-display");
+var pf = document.getElementById("player-fist");
+var cf = document.getElementById("computer-fist");
+var d = document.getElementById("rps-explanation");
 document.getElementById("player-score").innerHTML = playerScore;
 document.getElementById("computer-score").innerHTML = computerScore;
 document.getElementById("matches-num").innerHTML = matches;
+var winsults = ["Wow, well done, you beat a fucking computer, you should feel real proud of yourself.", 
+	"Don't let this win go to your head...you are playing Rock-Paper-Scissors on a computer...by yourself.", 
+	"Well done!  You don't completely suck!", 
+	"The computer probably let you win..."];
+
+var lossults = ["Ha! YOU SUCK!",
+	"Man, I bet you were up all night, working on that one!",
+	"Some babies were dropped on their heads but you were clearly thrown at a wall.",
+	"YOU LOST!  THE EARTH IS FULL GO HOME.",
+	"Wow, you are absolutely lousy at this...it's a KID'S GAME!",
+	"I'd love to insult you but now it's just getting sad"];
+
+var tiesults = ["Eh, you got lucky",
+	"Yay, a tie, that's sort of a big deal for you, huh?",
+	"It's a tie, bitches!!",
+	"You tied."]
+
+var winFunction = function(){
+	d.innerHTML = winsults[Math.floor(Math.random()*winsults.length)]
+  playerScore += 1;
+  matches += 1;
+};
+
+var lossFunction = function(){
+	d.innerHTML = lossults[Math.floor(Math.random()*lossults.length)];
+  computerScore += 1;
+  matches += 1;
+};
+
+var tieFunction = function(){
+	d.innerHTML = tiesults[Math.floor(Math.random()*tiesults.length)];
+  matches += 1;
+  playerScore += 0.5;
+  computerScore += 0.5;
+};
+
+	$(".select").click(function(){
+		
+		pf.className = "animated-fist fa fa-hand-rock-o fa-4x";
+		cf.className = "animated-fist fa fa-hand-rock-o fa-4x";
+		playerChoice = $(this).attr("id");
+		var fists = $(".animated-fist")
+		var up = 300;
+		var down = 200;
+		fists.animate({top: '-=50px'},up);
+		fists.animate({top: '+=100px'},down, "swing");
+		fists.animate({top: '-=100px'},up);
+		fists.animate({top: '+=100px'},down, "swing");
+		fists.animate({top: '-=100px'},up);
+		fists.animate({top: '+=100px'},down, "swing");
+		fists.animate({top: '-=100px'},up);
+		fists.animate({top: '+=50px'},down, "swing");
+
+		//setTimeout seems to need the function(){ }; I can't just put in the function that I want to call
+		setTimeout(function(){ playerSwitch() }, up*4+down*4);
+	});
 
 
-// This function is activated when a player clicks on one of the
-var rockpaperscissors = function(item) {
-	player = item;
-	playerChoice = item.getAttribute("id");
+var playerSwitch = function() {
+	switch (playerChoice) {
+		case "rock":
+			pf.className = "animated-fist fa fa-hand-rock-o fa-4x";
+			// console.log("rock");
+			break;
+		case "paper":
+			pf.className = "animated-fist fa fa-hand-paper-o fa-4x";
+			// console.log("paper");
+			break;
+		case "scissors":
+			pf.className = "animated-fist fa fa-hand-scissors-o fa-4x";
+			// console.log("scissors");
+	};
 	computerPick();
 }
 
+
 var computerPick = function() {
   var computerChoice = Math.random();
+  
   if (computerChoice < 0.34) {
     computerChoice = "rock";
-    var d = document.getElementById("computer-display");
-    d.className += "fa fa-hand-rock-o fa-4x";
-
+    // var d = document.getElementById("computer-display");
+    // d.className = "fa fa-hand-rock-o fa-4x";
+    cf.className = "animated-fist fa fa-hand-rock-o fa-4x";    
   } else if(computerChoice <= 0.67) {
     computerChoice = "paper";
-    var d = document.getElementById("computer-display");
-    d.className += "fa fa-hand-paper-o fa-4x";
+    // var d = document.getElementById("computer-display");
+    // d.className = "fa fa-hand-paper-o fa-4x";
+    cf.className = "animated-fist fa fa-hand-paper-o fa-4x"; 
   } else {
     computerChoice = "scissors";
-    var d = document.getElementById("computer-display");
-    d.className += "fa fa-hand-scissors-o fa-4x";
+    // var d = document.getElementById("computer-display");
+    // d.className = "fa fa-hand-scissors-o fa-4x";
+    cf.className = "animated-fist fa fa-hand-scissors-o fa-4x";
   } 
   compare(playerChoice,computerChoice)
 }
 
 var compare = function(choice1,choice2){
+	// Comparison for a tie
   if (choice1 === choice2) {
-    var d = document.getElementById("rps-explanation")
-    d.innerHTML = "It's a tie!  Try again!"
-    matches += 1;
-    playerScore += 0.5;
-    computerScore += 0.5;
+  	tieFunction();
+    // d.innerHTML = "It's a tie!  Try again!"
+    // matches += 1;
+    // playerScore += 0.5;
+    // computerScore += 0.5;
   } 
 
+  // Comparison for Player's Choice = Rock
 	else if (choice1 === "rock") {
   	if (choice2 === "scissors") {
-    var d = document.getElementById("rps-explanation");
-    d.innerHTML = "You win!  Great job!";
-    playerScore += 1;
-    matches += 1;
+    // d.innerHTML = "You win!  Great job!";
+    // d.innerHTML = winsults[Math.floor(Math.random()*winsults.length)]
+    // playerScore += 1;
+    // matches += 1;
+    	winFunction();
   	} else {
-    var d = document.getElementById("rps-explanation")
-    d.innerHTML = "You lose...how pathetic!!"
-    computerScore += 1;
-    matches += 1;
+    // d.innerHTML = "You lose...how pathetic!!"
+    // computerScore += 1;
+    // matches += 1;
+    	lossFunction();
  	 	};
  	} 
-  	
+  
+  // Comparison for Player's Choice = Paper
 	else if (choice1 === "paper") {
 	  if (choice2 === "rock") {
-	    var d = document.getElementById("rps-explanation")
-	    d.innerHTML = "You win!  Great job!"
-	    playerScore += 1;
-	    matches += 1;
+	    // d.innerHTML = "You win!  Great job!"
+	    // playerScore += 1;
+	    // matches += 1;
+	    winFunction();
 	  } else {
-	    var d = document.getElementById("rps-explanation")
-	    d.innerHTML = "You lose...how pathetic!!"
-	    computerScore += 1;
-	    matches += 1;
+	    // d.innerHTML = "You lose...how pathetic!!"
+	    // computerScore += 1;
+	    // matches += 1;
+	    lossFunction();
   		};
   } 
 
+  // Comparison for Player's Choice = Scissors
   else if (choice1 === "scissors") {
     if (choice2 === "paper") {
-      var d = document.getElementById("rps-explanation")
-      d.innerHTML = "You win!  Great job!"
-      playerScore += 1;
-      matches += 1;
+      // d.innerHTML = "You win!  Great job!"
+      // playerScore += 1;
+      // matches += 1;
+      winFunction();
     } else {
-      var d = document.getElementById("rps-explanation")
-      d.innerHTML = "You lose...how pathetic!!"
-      computerScore += 1;
-      matches += 1;
+      // d.innerHTML = "You lose...how pathetic!!"
+      // computerScore += 1;
+      // matches += 1;
+      lossFunction();
     };
-
   };
   document.getElementById("player-score").innerHTML = playerScore;
   document.getElementById("computer-score").innerHTML = computerScore;
@@ -93,116 +172,8 @@ var compare = function(choice1,choice2){
 };
 
 
-// var playAgain = function(){
-//   if (playerChoice = "") {
-//     alert("Waiting on you, slick...(make a fucking selection!)")
-//   } else {
-//     a = player.getAttribute("id");
-//     d = document.getElementById(a);
-//     d.style.backgroundColor = "";
-//     d.style.color = ""
-//     document.getElementById("rps-comp").className = "";
-//     document.getElementById("rps-result-under").innerHTML = "";
-//   }
-// }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var rockpaperscissors = function(item) {
-//   player = item;
-//   playerChoice = item.getAttribute("id");
-//   document.getElementById(playerChoice).style.backgroundColor = "black";
-//   document.getElementById(playerChoice).style.color = "blue";
-//   document.getElementById(playerChoice).style.borderRadius = "5px";
-
-//   // Timeout before revealing computer's pick  
-//   setTimeout(function(){computerPick()},500);
-  
-// }
-
-// var computerPick = function() {
-//   var computerChoice = Math.random();
-//   if (computerChoice < 0.34) {
-//     computerChoice = "rock";
-//     var d = document.getElementById("rps-comp");
-//     d.className += "fa fa-hand-rock-o fa-4x";
-
-//   } else if(computerChoice <= 0.67) {
-//     computerChoice = "paper";
-//     var d = document.getElementById("rps-comp");
-//     d.className += "fa fa-hand-paper-o fa-4x";
-//   } else {
-//     computerChoice = "scissors";
-//     var d = document.getElementById("rps-comp");
-//     d.className += "fa fa-hand-scissors-o fa-4x";
-//   } 
-//   compare(playerChoice,computerChoice)
-// }
-
-// var compare = function(choice1,choice2){
-//   if (choice1 === choice2) {
-//     var d = document.getElementById("rps-result-under")
-//     d.innerHTML = "It's a tie!  Try again!"
-//     matches += 1;
-//     playerScore += 0.5;
-//     computerScore += 0.5;
-//   } else if (choice1 === "rock") {
-//     if (choice2 === "scissors") {
-//       var d = document.getElementById("rps-result-under");
-//       d.innerHTML = "You win!  Great job!";
-//       playerScore += 1;
-//       matches += 1;
-//     } else {
-//       var d = document.getElementById("rps-result-under")
-//       d.innerHTML = "You lose...how pathetic!!"
-//       computerScore += 1;
-//       matches += 1;
-//     };
-
-//   } else if (choice1 === "paper") {
-//     if (choice2 === "rock") {
-//       var d = document.getElementById("rps-result-under")
-//       d.innerHTML = "You win!  Great job!"
-//       playerScore += 1;
-//       matches += 1;
-//     } else {
-//       var d = document.getElementById("rps-result-under")
-//       d.innerHTML = "You lose...how pathetic!!"
-//       computerScore += 1;
-//       matches += 1;
-//     };
-//   } else if (choice1 === "scissors") {
-//     if (choice2 === "paper") {
-//       var d = document.getElementById("rps-result-under")
-//       d.innerHTML = "You win!  Great job!"
-//       playerScore += 1;
-//       matches += 1;
-//     } else {
-//       var d = document.getElementById("rps-result-under")
-//       d.innerHTML = "You lose...how pathetic!!"
-//       computerScore += 1;
-//       matches += 1;
-//     };
-
-//   };
-//   document.getElementById("player-score").innerHTML = playerScore;
-//   document.getElementById("computer-score").innerHTML = computerScore;
-//   document.getElementById("matches-num").innerHTML = matches;
-
-// };
 
